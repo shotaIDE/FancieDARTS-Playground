b9f148e29ded594312955e99c8c946f46d10ed5e
DAJ-NET\shota.ide <shota.ide@daj.co.jp>
2018-06-21 18:57:30 +0900

    #11 タクソノミーの説明が設定されていない場合は、説明フィールドを表示しないように修正

--- app/wp-content/themes/dp-fancie-note-child/article-loop-member_post.php
+++ app/wp-content/themes/dp-fancie-note-child/article-loop-member_post.php
@@ -135,13 +135,16 @@ function dp_get_infeed_ads($arr_params = array(
 // ***********************************
 // for FancieDARTS [START]: タクソノミーの説明を表示
 // ***********************************
+$term_code = '';
 $term = get_queried_object();
 $term_name = $term->name;
 $term_description = term_description(); // 整形されたデータ取得のため、$term->description は利用しない
-$term_code = sprintf('<div class="widget-content bottom clearfix"><div id="dprecentcustompostswidget-2" class="widget-box dp_recent_custom_posts_widget slider_fx"><h3 class="inside-title wow fadeInLeft" style="visibility: visible; animation-name: fadeInLeft;"><span>%s</span></h3>
-	<div class="entry entry-content" style="padding-left: 10px">%s</div></div></div>',
-	$term_name,
-	$term_description);
+if ($term_description !== '') {
+	$term_code = sprintf('<div class="widget-content bottom clearfix"><div id="dprecentcustompostswidget-2" class="widget-box dp_recent_custom_posts_widget slider_fx"><h3 class="inside-title wow fadeInLeft" style="visibility: visible; animation-name: fadeInLeft;"><span>%s</span></h3>
+		<div class="entry entry-content" style="padding-left: 10px">%s</div></div></div>',
+		$term_name,
+		$term_description);
+}
 echo $term_code;
 // ***********************************
 // for FancieDARTS [END]: タクソノミーの説明を表示
