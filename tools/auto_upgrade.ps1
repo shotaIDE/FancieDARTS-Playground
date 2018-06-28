481a52dba97e955a84c70105e10ea6e0adcb4add
Shota IDE <ide.shota.dev@gmail.com>
2018-06-29 00:31:19 +0900

    #12 テスト失敗時は処理を中断するよう修正

--- tools/auto_upgrade.ps1
+++ tools/auto_upgrade.ps1
@@ -3,8 +3,12 @@ python admin_upgrade.py 'http://localhost' 'settings_dev.txt'
 # 開発環境：ファイル修正、文字列置換スクリプトによる
 docker exec darts_wordpress_1 php /tmp/fanciedarts/rewrite_after_upgrade.php
 # 開発環境：自動テスト
-$testResult = (python view_test.py 'http://localhost' 'settings_dev.txt')
-# TODO:ここで引っかかった場合は、以下処理を中断する
+python view_test.py 'http://localhost' 'settings_dev.txt'
+if (!$?) {
+    # テスト失敗
+    echo '[ERROR] test failed in develop'
+    exit
+}
 
 # 本番環境：アップグレード
 python admin_upgrade.py 'http://192.168.10.70/fanciedarts' 'settings_public.txt'
@@ -26,5 +30,9 @@ foreach ($file in $files) {
 } 
 
 # 開発環境：自動テスト
-$testResult = (python view_test.py 'http://192.168.10.70/fanciedarts' 'settings_public.txt')
-# TODO:ここで引っかかった場合は、アラートを発生させる
+python view_test.py 'http://192.168.10.70/fanciedarts' 'settings_public.txt'
+if (!$?) {
+    # テスト失敗
+    echo '[ERROR] test failed in PUBLIC'
+    exit
+}
