0e7ca7b28e122f5754d42ffec91e4450c1d4d936
Shota Ide <shota.ide@daj.co.jp>
2019-01-17 16:34:13 +0900

    #22 WordPressの初期設定に、自動更新のオフとパーマリンク設定更新を追加

--- entrypoint-initapp.sh
+++ entrypoint-initapp.sh
@@ -2,7 +2,10 @@
 wp option update blogname 'DARTS-Dev'
 wp option update siteurl 'http://localhost:10780/fanciedarts'
 wp option update home 'http://localhost:10780/fanciedarts'
-wp rewrite flush
+wp config set \
+    AUTOMATIC_UPDATER_DISABLED true --raw --type=constant
+wp rewrite structure \
+    '/%category%/%post_id%/'
 
 # 開発環境用のAdminユーザを作成
 wp user create admin admin@example.com --role=administrator --user_pass=z
